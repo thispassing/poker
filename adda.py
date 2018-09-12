ff41a9ebc67bac1874f311037fbeaef536a96b01
thispassing <thispassing@users.noreply.github.com>
2018-09-12 12:33:17 -0400

    cleaned up some of the variable names

--- adda.py
+++ adda.py
@@ -1,18 +1,18 @@
-#importing datetime and defining time
+# importing datetime and defining current date
 
 from datetime import datetime
 today = datetime.now().strftime("%Y-%b-%d")
 
-#calculating new balance
+# making definition for new balance
 
 def calculate(totalBalance):
     chop = 600000
     tax = 218543
-    newBalance = (int(totalBalance) - chop - tax)
+    newBalance = totalBalance - chop - tax
     newBalance = "{:,}".format(newBalance)
     return (newBalance)
 
-#receiving user input for current balance and giving new balance as output
+# receiving user input for current balance and giving new balance as output
 
 balance = int(input("Type in your current balance: "))
-print("Current balance is: ", calculate(balance), "on", today)
+print("New balance is: ", calculate(balance), "on", today)
